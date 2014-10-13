e9cedf95392c1a9809aefe787dc2891a759e2e5f
Max Wofford <max.speed.wofford@gmail.com>
2014-10-13 15:36:13 -0600

    new paste path

--- app/controllers/paste_controller.rb
+++ app/controllers/paste_controller.rb
@@ -2,10 +2,14 @@ class PasteController < ApplicationController
 
   def new
     @paste = Paste.create
-    redirect_to "/#{@paste.hash}"
+    redirect_to "/#{@paste.token}"
   end
   def edit
     @paste = Paste.where(token: params[:token])
+  end
+  def update
+    @paste = Paste.where(token: params[:token])
+    @paste.content = params[:content]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  root 'paste#new'
-  get '/:hash' => 'paste#edit'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'paste#new', as: :new_paste
+  get '/:token' => 'paste#edit'
+  put '/:token' => 'paste#update'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
